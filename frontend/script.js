c0fb56183c7e5028d8bdbbedda6972b5cf12f4b1
Vitoria <v936955@gmail.com>
2023-08-15 18:05:03 -0300

    html e css

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-function trocarImagem() {
-    var imagem = document.getElementById("imagem");
-    
-    if (imagem.src.endsWith("https://a.imagem.app/bmMIwr.jpegimagem1.jpg")) {
-      imagem.src = "https://a.imagem.app/bmMZkP.jpeg";
-    } else {
-      imagem.src = "https://a.imagem.app/bmMIwr.jpeg";
-    }
-  }
